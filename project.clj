21c3240d737221c016e38d1ceafeb5ffb8b45fbf
Trever Anderson <Trever.Anderson@avantas.com>
2025-04-14 22:58:18 -0500

    loaded ecs client

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [com.cognitect.aws/api "0.8.735"]
+                 [com.cognitect.aws/endpoints "871.2.30.22"]
+                 [com.cognitect.aws/s3 "871.2.30.22"]
+                 [com.cognitect.aws/ecs "871.2.30.22"]]
   :repl-options {:init-ns ecsctl.core}
   :main ecsctl.core
-  :skip-aot ecsctl.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  ;:skip-aot ecsctl.core:target-path "target/%s"
+  ;:profiles {:uberjar {:aot :all}}
+  )
--- src/ecsctl/core.clj
+++ src/ecsctl/core.clj
@@ -1,6 +1,29 @@
-(ns ecsctl.core)
+(ns ecsctl.core
+  ;;(:require [arc-solver2.image-utils :as iu])
+  (:require [clojure.set :as cset])
+  (:require [cognitect.aws.client.api :as aws]))
+
+(def s3 (aws/client {:api :s3}))
+
+(defn test-func []
+  (aws/ops aws/client))
+
+;;ecsctl list tasks
+;;ecsctl tasks list
+
+(def verb-to-noun {:list #{"tasks"}})
+
+(defn valid-command? [verb noun]
+  )
+
+(defn va-test [args verbs nouns]
+  (cond
+    (not= (first args) "ecsctl") false
+    (nil? (verbs (second args))) false
+    (nil? (nouns (nth args 2))) false
+    :else true))
 
 (defn -main
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [x & args]
+  (println (map #(type %) (flatten [x args])))
+  (println "type of args: " (type args) " len args: " (count args)))
