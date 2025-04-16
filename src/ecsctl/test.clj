44c8f1c7e80d2f50b15e6044adeb1fd60d4cc9d1
Trever Anderson <Trever.Anderson@avantas.com>
2025-04-15 21:38:05 -0500

    played around more with ecs client

--- src/ecsctl/test.clj
+++ src/ecsctl/test.clj
@@ -4,6 +4,11 @@
 
 (def ecs (aws/client {:api :ecs}))
 
-(defn test-client []
-  (let [tc (test/client {:api :ecs :ops {:ListTasks {:response ["arn"]}}})]
-    (aws/invoke tc {:op :ListTasks :request {:family "hey"}})))
+(def literal-test-client (test/client {:api :ecs :ops {:ListTasks
+                                                       {:response ["literal-example-some-arn"]}}}))
+
+(def func-test-client (test/client {:api :ecs :ops {:ListTasks (fn [{:keys [op request] :as op-map}]
+                                                                 {:response ["func-example-some-arn"]})}}))
+
+(defn list-tasks-test-call [test-client]
+    (aws/invoke test-client {:op :ListTasks :request {:family "hey"}}))
