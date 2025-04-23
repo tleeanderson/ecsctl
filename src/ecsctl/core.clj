3f6386a4e4fb6ba189233d270157781f51207a4a
Trever Anderson <Trever.Anderson@avantas.com>
2025-04-22 23:09:59 -0500

    rewrote command validation

--- src/ecsctl/core.clj
+++ src/ecsctl/core.clj
@@ -9,32 +9,36 @@
 (def prog-name "ecsctl")
 (def usage (str prog-name " [verb] [noun]"))
 
+;example inputs
+(def gi ["ecsctl" "list" "tasks"])
+
 (defn keyword-from-args [cli-args]
   (keyword (clojure.string/join (mapv clojure.string/capitalize cli-args))))
 
-(defn valid-args?
+(defn valid-keyword
   ([cli-args]
-   (valid-args? cli-args ecs-ops))
-  ([cli-args ops]
-   (contains? ops (keyword-from-args cli-args))))
+   (valid-keyword cli-args ecs-ops))
+  ([[_ & args] ops]
+   (let [kw (keyword-from-args args)]
+     (when (contains? ops kw)
+       kw))))
 
-(defn valid-command? [cli-args]
-  (cond
-    ;TODO might not need this check
-    (not= (first cli-args) prog-name) (do
-                                        (println "error: command must start with" prog-name)
-                                        (println usage)
-                                        false)
-    (false? (valid-args? (rest cli-args))) (do
-                                             (if (empty? (rest cli-args))
-                                               (println "error: no arguments provided" )
-                                               (println "error:" (clojure.string/join " " (rest cli-args))
-                                                        "are not valid arguments"))
-                                             (println usage)
-                                             false)
-    :else (do (println cli-args "is a valid command")
-              true)))
+(defn valid-command [cli-args]
+  (let [args (valid-keyword cli-args)]
+    (cond
+      ;TODO might not need this check
+      (not= (first cli-args) prog-name) (do
+                                          (println "error: command must start with" prog-name)
+                                          (println usage))
+      (nil? args) (do
+                    (if (empty? (rest cli-args))
+                      (println "error: no arguments provided" )
+                      (println "error:" (clojure.string/join " " (rest cli-args))
+                               "are not valid arguments"))
+                    (println usage))
+      :else (do (println cli-args "is a valid command")
+                args))))
 
 (defn -main
   [fa & args]
-  (valid-command? (apply list fa args)))
+  (valid-command (apply list fa args)))
