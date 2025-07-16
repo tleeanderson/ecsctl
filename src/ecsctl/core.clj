679ec9a2b04b43a8e8a2ba20c0c330744bde3d5a
Trever Anderson <Trever.Anderson@avantas.com>
2025-07-15 22:55:24 -0500

    list tasks executes, returns nothing

--- src/ecsctl/core.clj
+++ src/ecsctl/core.clj
@@ -1,6 +1,59 @@
-(ns ecsctl.core)
+(ns ecsctl.core
+  (:gen-class)
+  (:require [clojure.string])
+  (:require [cognitect.aws.client.api :as aws]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;examples
+;(aws/invoke s3 {:op :ListBuckets})
+
+(def ecs-client (aws/client {:api :ecs}))
+(def ecs-ops (aws/ops ecs-client))
+(def prog-name "ecsctl")
+(def usage (str prog-name " [verb] [noun]"))
+
+;example inputs
+(def gi ["ecsctl" "list" "tasks"])
+
+(defn keyword-from-args [cli-args]
+  (keyword (clojure.string/join (mapv clojure.string/capitalize cli-args))))
+
+(defn valid-keyword
+  ([cli-args]
+   (valid-keyword cli-args ecs-ops))
+  ([[_ & args] ops]
+   (let [kw (keyword-from-args args)]
+     (when (contains? ops kw)
+       kw))))
+
+(defn valid-command [cli-args]
+  (let [args (valid-keyword cli-args)]
+    (cond
+      ;TODO might not need this check
+      (not= (first cli-args) prog-name) (do
+                                          (println "error: command must start with" prog-name)
+                                          (println usage))
+      (nil? args) (do
+                    (if (empty? (rest cli-args))
+                      (println "error: no arguments provided" )
+                      (println "error:" (clojure.string/join " " (rest cli-args))
+                               "are not valid arguments"))
+                    (println usage))
+      :else (do (println cli-args "is a valid command")
+                args))))
+
+(defn handle-invoke [invoke-ret]
+  (if (= (invoke-ret :cognitect.anomalies/category) :cognitect.anomalies/fault)
+    (do
+      (println "error message: " (invoke-ret :cognitect.anomalies/message) "\n"
+               (invoke-ret :cognitect.aws.util/throwable)))
+    (println invoke-ret)))
+
+(defn run-cmd [args]
+  (let [cmd (valid-command args)]
+    (when (some? cmd)
+      (handle-invoke (aws/invoke ecs-client {:op cmd})))))
+
+(defn -main
+  ;type ArraySeq
+  [fa & args]
+  (run-cmd (apply list fa args)))
---
+++ src/ecsctl/test.clj
@@ -0,0 +1,12 @@
+(ns ecsctl.test
+  (:require [cognitect.aws.client.test-double :as test]
+           [cognitect.aws.client.api :as aws]))
+
+(def literal-test-client (test/client {:api :ecs :ops {:ListTasks
+                                                       {:response ["literal-example-some-arn"]}}}))
+
+(def func-test-client (test/client {:api :ecs :ops {:ListTasks (fn [{:keys [op request] :as op-map}]
+                                                                 {:response ["func-example-some-arn"]})}}))
+
+(defn list-tasks-test-call [test-client]
+    (aws/invoke test-client {:op :ListTasks :request {:family "hey"}}))
